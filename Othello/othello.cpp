49d946e92176c617c6c3a6360cc4117562bd13e4
Landon Epps <landon.epps@gmail.com>
2013-04-19 21:45:39 -0500

    Working valid move detection.

--- Othello/othello.cpp
+++ Othello/othello.cpp
@@ -10,69 +10,85 @@
 #include <vector>
 #include "othello.h"
 
-int main(int argc, const char * argv[])
-{
-    
-    return 0;
-}
+const pair<int, int> adjacentMoves[8] = {make_pair(-1, 0), make_pair(-1, 1),
+make_pair(0 , 1), make_pair(1 , 1), make_pair(1 , 0), make_pair(1 , -1),
+make_pair(0, -1), make_pair(-1, -1)};
 
+/*
 struct moveNode {
     moveNode *parent;
     vector<moveNode> children;
     int score;
     bool max;
     pair<int, int> move;
-    moveNode(int s, bool m){
+    moveNode(int s, int m){
         score = s;
         max = m;
     }
 };
+*/
+struct moveInfo {
+    int score;
+    pair<int, int> move;
+    moveInfo (int s, pair<int, int> m) {
+        score = s;
+        move = m;
+    }
+};
 
-
-void findMoves(int color, vector<pair<int, int> > &blackMoves, vector<pair<int, int> > &whiteMoves, Board b) {
-    vector<pair<int, int> > *opponentsMoves;
+vector<moveInfo> findMoves(int color, vector<pair<int, int> > &blackPieces, vector<pair<int, int> > &whitePieces, Board b) {
+    vector<pair<int, int> > *opponentsPieces;
+    vector<moveInfo> moves;
     int opponent;
     if (color == WHITE) {
         opponent = BLACK;
-        opponentsMoves = &blackMoves;
+        opponentsPieces = &blackPieces;
     } else {
         opponent = WHITE;
-        opponentsMoves = &whiteMoves;
+        opponentsPieces = &whitePieces;
     }
-
-    for (int i = 0; i < opponentsMoves->size(); i++) {
-        for(int j = 0; j < 8; < j++) {
-            pair<int,int> move = (*opponentsMoves)[j] + enumNum[j];
-            if (move == OPEN) {
-                if (i > 0) {
-                    if (j > 0) {
-                        if (b[i - 1][j - 1] == opponent);
-                    }
-                    if (b[i - 1][j] == opponent);
-                    if (j < b.getCol() - 1) {
-                        if (b[i - 1][j + 1]) == opponent;
-                    }
-                }
-                if (i < b.getRow() - 1) {
-                    if (j > 0) {
-
-                    }
-                    if (j < b.getCol() - 1) {
-                        
-                    }
-                }
-                if (j > 0) {
-
-                }
-                if (j < b.getCol() - 1) {
+    // for all opponents pieces
+    for (int i = 0; i < opponentsPieces->size(); i++) {
+        // for all directions
+        for(int j = 0; j < 8; j++) {
+            // make a move for each direction in adjacentMoves
+            pair<int,int> move = make_pair((*opponentsPieces)[i].first + adjacentMoves[j].first,
+                                           (*opponentsPieces)[i].second + adjacentMoves[j].second);
+            // if that move is on the board and is open
+            if (move.first >= 0 && move.first < b.getRow() &&
+                move.second >= 0 && move.second < b.getCol() &&
+                b[move.first][move.second] == OPEN) {
+                // keep track of score
+                int score = 0;
+                // for all directions
+                for (int k = 0; k < 8; k++) {
+                    // keep track of score for each direction
+                    int tempScore = 0;
+                    pair<int, int> nextDirection = make_pair(move.first + adjacentMoves[k].first,
+                                                             move.second + adjacentMoves[k].second);
                     
+                    while (nextDirection.first >= 0 && nextDirection.first < b.getRow() &&
+                           nextDirection.second >= 0 && nextDirection.second < b.getCol() &&
+                           b[nextDirection.first][nextDirection.second] == opponent) {
+                        tempScore++;
+                        nextDirection = make_pair(nextDirection.first + adjacentMoves[k].first,
+                                                  nextDirection.second + adjacentMoves[k].second);
+                    }
+                    if (nextDirection.first >= 0 && nextDirection.first < b.getRow() &&
+                        nextDirection.second >= 0 && nextDirection.second < b.getCol() &&
+                        b[nextDirection.first][nextDirection.second] == color) {
+                        score += tempScore;
+                    }
+                }
+                if (score > 0) {
+                    moves.push_back(moveInfo(score, move));
                 }
             }
         }
     }
-    return;
+    return moves;
 }
-
+/*
 pair<int, int> generateTree(moveNode *root, vector<int, int> moves) {
 
     return make_pair(int(), int());
@@ -84,4 +100,40 @@ pair<int, int> TeamNULLPlayer::move(Board b){
     moveNode* root;
 
     return nextMove;
+}
+*/
+int main(int argc, const char * argv[])
+{
+    Board board;
+    vector<pair<int, int> > blackPieces;
+    vector<pair<int, int> > whitePieces;
+    blackPieces.push_back(make_pair(1, 0));
+    blackPieces.push_back(make_pair(1, 2));
+    blackPieces.push_back(make_pair(2, 3));
+    blackPieces.push_back(make_pair(3, 2));
+    whitePieces.push_back(make_pair(0, 0));
+    whitePieces.push_back(make_pair(1, 1));
+    whitePieces.push_back(make_pair(2, 2));
+    whitePieces.push_back(make_pair(3, 3));
+
+
+    /*
+     grid[0][0] = WHITE;
+     grid[1][1] = WHITE;
+     grid[2][2] = WHITE;
+     grid[3][3] = WHITE;
+     grid[1][0] = BLACK;
+     grid[1][2] = BLACK;
+     grid[2][3] = BLACK;
+     grid[3][2] = BLACK;
+     */
+
+    vector<moveInfo> theMoves;
+    theMoves = findMoves(1, blackPieces, whitePieces, board);
+
+    for (int i = 0; i < theMoves.size(); i++) {
+        cout << "Move #" << i + 1 << ": (" << theMoves[i].move.first << ", " << theMoves[i].move.second << ")" << endl;
+        cout << "Score: " << theMoves[i].score << endl;
+    }
+    return 0;
 }
--- Othello/othello.h
+++ Othello/othello.h
@@ -17,7 +17,7 @@ const int OPEN  = 0;
 const int BLACK = 1;
 const int WHITE = 2;
 
-
+/*
 class Board{
 private:
     int row, col;
@@ -26,6 +26,49 @@ public:
     int getRow();
     int getCol();
     int* operator[](int n);
+};*/
+
+class Board {
+private:
+    int row, col;
+    int **grid;
+public:
+    int getRow() {
+        return row;
+    }
+    int getCol() {
+        return col;
+    }
+    Board(int r = 8, int c = 8) {
+        row = r;
+        col = c;
+        grid = new int*[row];
+        for (int i = 0; i < row; i++) {
+            grid[i] = new int[col];
+            for (int j = 0; j < col; j++) {
+                grid[i][j] = OPEN;
+            }
+        }
+        grid[0][0] = WHITE;
+        grid[1][1] = WHITE;
+        grid[2][2] = WHITE;
+        grid[3][3] = WHITE;
+        grid[1][0] = BLACK;
+        grid[1][2] = BLACK;
+        grid[2][3] = BLACK;
+        grid[3][2] = BLACK;
+
+        
+    }
+    ~Board() {
+        for (int i = 0; i < row; i++) {
+            //delete [] grid[i];
+        }
+        //delete [] grid;
+    }
+    int* operator[](int n) {
+        return grid[n];
+    }
 };
 
 class Player{
