79f804a27c5d1f396a5f9a139ac266470dc9d4ce
Landon Epps <landon.epps@gmail.com>
2013-05-09 12:06:39 -0500

    Winning Othello algorithm.

--- Othello/TeamNULL.cpp
+++ Othello/TeamNULL.cpp
@@ -4,7 +4,10 @@
  Description: This algorithm determines the optimal moves for a game of
  Othello. While this algorithm doesn't solve the game of Othello (since
  right now, Othello is considered an unsolved game), it implements a
- strategy which will calculate winning moves for the game.
+ strategy which will calculate winning moves for the game. We use a strategy
+ that limits the available moves of the opponent, while maximizing our
+ available moves at the end of the game. We take into account special moves
+ such as corner pieces, which are always the best move if available.
  Created: 4/16/2013
  Modified: 4/29/2013
  Due: 4/29/2013
@@ -43,7 +46,8 @@ void printBoard(Board b) {
 // post: the pieces/oppPiece vectors and board is unchanged                   *
 // returns: a set containing the possible moves for a player                  *
 //*****************************************************************************
-set<Move> findMoves(int color, vector<pair<int, int> > &oppPiece, Board &b) {
+set<Move> findMoves(int color, pair<int, int> &realMove,
+                    vector<pair<int, int> > const &oppPiece, Board &b) {
     set<Move> moves;
     int opponent;
     if (color == WHITE) {
@@ -52,52 +56,111 @@ set<Move> findMoves(int color, vector<pair<int, int> > &oppPiece, Board &b) {
         opponent = WHITE;
     }
     // for all opponents pieces
-    for (int i = 0; i < oppPiece.size(); i++) {
+    for (int piece = 0; piece < oppPiece.size(); piece++) {
         // for all directions
-        for(int j = 0; j < 8; j++) {
+        for(int dir1 = 0; dir1 < 8; dir1++) {
             // make a move for each direction in adjMovs
-            pair<int,int> move = make_pair(oppPiece[i].first +
-                                           adjMovs[j].first,
-                                           oppPiece[i].second +
-                                           adjMovs[j].second);
+            pair<int,int> move = make_pair(oppPiece[piece].first +
+                                           adjMovs[dir1].first,
+                                           oppPiece[piece].second +
+                                           adjMovs[dir1].second);
             // if that move is on the board and is open
             if (move.first >= 0 && move.first < b.getRow() &&
                 move.second >= 0 && move.second < b.getCol() &&
                 b[move.first][move.second] == OPEN) {
                 // keep track of score
                 int score = 0;
-                Board newBoard = b;
-                newBoard[move.first][move.second] = color;
                 // for all directions
-                for (int k = 0; k < 8; k++) {
+                for (int dir2 = 0; dir2 < 8; dir2++) {
                     // keep track of score for each direction
-                    int tempScore = 0;
-                    Board tempBoard = newBoard;
+                    int dirScore = 0;
                     pair<int, int> nextDir;
-                    nextDir.first = move.first + adjMovs[k].first;
-                    nextDir.second = move.second
-                    + adjMovs[k].second;
+                    nextDir.first = move.first + adjMovs[dir2].first;
+                    nextDir.second = move.second + adjMovs[dir2].second;
 
                     while (nextDir.first >= 0 && nextDir.first < b.getRow() &&
                            nextDir.second >= 0 && nextDir.second < b.getCol() &&
                            b[nextDir.first][nextDir.second] == opponent) {
-                        tempScore++;
-                        tempBoard[nextDir.first][nextDir.second] = color;
-                        nextDir = make_pair(nextDir.first + adjMovs[k].first,
-                                            nextDir.second + adjMovs[k].second);
+                        dirScore++;
+                        nextDir = make_pair(nextDir.first + adjMovs[dir2].first,
+                                            nextDir.second + adjMovs[dir2].second);
                     }
                     if (nextDir.first >= 0 &&
                         nextDir.first < b.getRow() &&
                         nextDir.second >= 0 &&
                         nextDir.second < b.getCol() &&
                         b[nextDir.first][nextDir.second] == color) {
-                        score += tempScore;
-                        newBoard = tempBoard;
+                        score += dirScore;
                     }
                 }
+                // if the move is a valid move (it took an opponents piece)
                 if (score > 0) {
-                    Move tempMove(move, score, newBoard);
-                    moves.insert(tempMove);
+                    int cols = b.getCol();
+                    int rows = b.getRow();
+                    // if the move is a corner spot, set to highest priority
+                    if ((move.first == 0 && move.second == 0) ||
+                        (move.first == 0 && move.second == cols - 1) ||
+                        (move.first == rows - 1 && move.second == 0) ||
+                        (move.first == rows - 1 && move.second == cols - 1)) {
+                        score = -(cols * rows);
+                        realMove = move;
+                    }
+                    // if the move is the piece diagonal to the corner piece,
+                    // set to lowest priority
+                    else if ((move.first == 1 && move.second == 1) ||
+                             (move.first == 1 && move.second == cols - 2) ||
+                             (move.first == rows - 2 && move.second == 1) ||
+                             (move.first == rows - 2 && move.second == cols - 2)) {
+                        score = (cols * rows);
+                    }
+
+                    // if the move is adjacent to a corner spot and the corner
+                    // spot has a piece in it, prioritize the move as high
+                    // if the corner spot is open, prioritize to low
+                    else if (move.first == 0 && move.second == 1) {
+                        if (b[0][0] == OPEN) score = 3*rows;
+                        else score = 0;
+                    }
+                    else if (move.first == 0 && move.second == cols - 2) {
+                        if (b[0][cols-1] == OPEN) score = 3*rows;
+                        else score = 0;
+                    }
+                    else if (move.first == rows - 1 && move.second == 1) {
+                        if (b[rows-1][0] == OPEN) score = 3*rows;
+                        else score = 0;
+                    }
+                    else if (move.first == rows - 1 && move.second == cols - 2){
+                        if (b[rows-1][cols-1] == OPEN) score = 3*rows;
+                        else score = 0;
+                    }
+
+                    else if (move.first == 1 && move.second == 0) {
+                        if (b[0][0] == OPEN) score = 3*rows;
+                        else score = 0;
+                    }
+                    else if (move.first == 1 && move.second == cols - 1) {
+                        if (b[0][cols-1] == OPEN) score = 3*rows;
+                        else score = 0;
+                    }
+                    else if (move.first == rows - 2 && move.second == 0) {
+                        if (b[rows-1][0] == OPEN) score = 3*rows;
+                        else score = 0;
+                    }
+                    else if (move.first == rows - 2 && move.second == cols - 1){
+                        if (b[rows-1][cols-1] == OPEN) score = 3*rows;
+                        else score = 0;
+                    }
+
+                    // if the move is an edge move, prioritize it to high
+                    else if (move.first == 0 || move.first == rows - 1 ||
+                             move.second == 0 || move.second == cols - 1) {
+                        score = 0;
+                    }
+                    else {
+                        realMove = move;
+                    }
+
+                    moves.insert(Move(move, score));
                 }
             }
         }
@@ -115,7 +178,7 @@ set<Move> findMoves(int color, vector<pair<int, int> > &oppPiece, Board &b) {
 // post: the move reference passed is changed to the decided piece placement  *
 // returns: nothing                                                           *
 //*****************************************************************************
-Move TeamNULL::move(Board b, pair<int,int> &move) {
+void TeamNULL::move(Board b, pair<int,int> &move) {
     int color = getColor();
     int opponent;
     if (color == WHITE) {
@@ -124,32 +187,24 @@ Move TeamNULL::move(Board b, pair<int,int> &move) {
         opponent = WHITE;
     }
 
-    root = new moveNode;
-    root->color = opponent;
-    root->board = b;
-    root->move = make_pair(-1, -1);
+    vector<pair<int, int> > oppPieces;
+
     for (int i = 0; i < b.getRow(); i++) {
         for (int j = 0; j < b.getCol(); j++) {
-            if (b[i][j] == color) {
-                root->pieces.push_back(make_pair(i, j));
-            } else if (b[i][j] == opponent){
-                root->oppPiece.push_back(make_pair(i, j));
+            if (b[i][j] == opponent){
+                oppPieces.push_back(make_pair(i, j));
             }
         }
     }
 
-    set<Move> moves;
-    moves = findMoves(color, root->oppPiece, b);
+    set<Move> moves = findMoves(color, move, oppPieces, b);
 
     if (!moves.empty()) {
         move = moves.begin()->position;
-    } else {
-        Move noMoves(make_pair(-1, -1), 0, b);
-        return noMoves;
     }
-
-    return *(moves.begin());
 }
+
+
 
 int main(int argc, const char * argv[])
 {
@@ -166,12 +221,12 @@ int main(int argc, const char * argv[])
     for (int i = 0; i < 32; i++) {
         TeamNULL player1(BLACK);
         TeamNULL player2(WHITE);
-        Move moveObj = player1.move(board, move);
-        board = moveObj.board;
-        printBoard(board);
-        moveObj = player2.move(board, move);
-        board = moveObj.board;
-        printBoard(board);
+        player1.move(board, move);
+        // board = moveObj.board;
+        // printBoard(board);
+        player2.move(board, move);
+        // board = moveObj.board;
+        // printBoard(board);
     }
 
     return 0;
--- Othello/TeamNULL.h
+++ Othello/TeamNULL.h
@@ -19,6 +19,8 @@
 
 using namespace std;
 
+// start testing code
+
 const int OPEN  = 0;
 const int BLACK = 1;
 const int WHITE = 2;
@@ -96,61 +98,45 @@ public:
 class Player{
 private:
     int color;
-
+    
 public:
     Player(int c = BLACK) { color = c; }
     virtual ~Player() { };
     int getColor() { return color; }
     void setColor(int c) { color = c; }
     ///////// This needs to be changed back
-    //virtual void move(Board, pair<int,int>&) = 0;
+    virtual void move(Board, pair<int,int>&) = 0;
 };
+
+// end testing code
 
 class Move {
 public:
     pair<int, int> position;
     int score;
-    Board board;
     bool operator<(const Move &other) const {
         return (this->score < other.score);
     }
 
-    Move(pair<int, int> p, int s, Board const &b) {
+    Move(pair<int, int> p, int s) {
         position = p;
         score = s;
-        board = b;
     }
 
     Move(Move const &other) {
         position = other.position;
         score = other.score;
-        board = other.board;
     }
-};
-
-struct moveNode {
-    int color;
-    vector<moveNode> children;
-    Board board;
-    vector<pair<int, int> > pieces;
-    vector<pair<int, int> > oppPiece;
-    pair<int, int> move;
 };
 
 class TeamNULL: public Player
 {
-private:
-    moveNode *root;
-    Move findMax(Board b);
-    Move findMin(Board b);
 public:
-    TeamNULL(int c):Player(c) {
-        root = NULL;
-    }
+    TeamNULL(int c):Player(c) {;}
     //**************************************************************************
     // name: move                                                              *
     // description: finds a location to place a piece for the current move of  *
-    //     Othello given the board.                                            *
+    //              Othello given the board.                                   *
     // return type: void                                                       *
     // pre: the board information is valid and reflects the current state of   *
     //      the game.                                                          *
@@ -158,8 +144,10 @@ public:
     //       placement                                                         *
     // returns: nothing                                                        *
     //**************************************************************************
-    //void move(Board, pair<int,int>&);
-    Move move(Board, pair<int,int>&);
+    void move(Board, pair<int,int>&);
 };
+
+set<Move> findMoves(int color, pair<int, int> &realMove,
+                    vector<pair<int, int> > const &oppPiece, Board &b);
 
 #endif
